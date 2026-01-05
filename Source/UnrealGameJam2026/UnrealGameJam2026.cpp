99ef63922608efb26a136ea54da36804fae4607c
Gontarekt <Toddynator@hotmail.co.uk>
2026-01-05 18:45:19 +0000

    Initial Commit

---
+++ Source/UnrealGameJam2026/UnrealGameJam2026.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealGameJam2026.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealGameJam2026, "UnrealGameJam2026" );
